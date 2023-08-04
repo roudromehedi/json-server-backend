3138ccd7c3f08e5fe3647c612407109e8bff4b03
RoudroMehedi <roudro.m@gmail.com>
2023-08-05 01:33:18 +0200

    wip

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
-const jsonServer = require("json-server"); // importing json-server library
+const jsonServer = require("json-server");
+const cors = require("cors"); // Importing cors package
+
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 8000; //  chose port from here like 8080, 3001
+const port = process.env.PORT || 8000;
 
+server.use(cors()); // Enable CORS
 server.use(middlewares);
 server.use(router);
 
-server.listen(port);
+server.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
